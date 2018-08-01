1a6260e92ef5c0ad6224425b6ece38bf3f6cce0f
Luis Javier Montoro Castillo <luis@MacBook-Pro-de-Luis.local>
2018-08-01 12:24:33 +0200

    inicializacion uiengine

---
+++ node_modules/@uiengine/ui/dist/_assets/scripts/uiengine-inject.4feefa7dc8a2ec74cc98.js
@@ -0,0 +1,2 @@
+webpackJsonp([9],{"3Or/":function(n,c){},Kx2R:function(n,c,t){"use strict";t("3Or/")}},["Kx2R"]);
+//# sourceMappingURL=uiengine-inject.4feefa7dc8a2ec74cc98.js.map
---
+++ uiengine.config.js
@@ -0,0 +1,57 @@
+module.exports = {
+  // Project config: Defaults to name and version from package.json.
+  // Here you can overwrite it and add more custom properties.
+  // For a detailed documentation of all the options in here see
+  // https://uiengine.uix.space/basics/config/
+  name: 'Thonet',
+
+  // Base directories for the input, your raw source files:
+  // - components is the root of the directory containing the components
+  // - templates contains the variant preview and application templates
+  // - pages is the directory of the UIengine's site structure and page markdown files
+  // - data contains sample data that can be referenced in variants and pages
+  // - entities contains the optional entity definitions for the components
+  source: {
+    components: './src/components',
+    templates: './src/templates',
+    pages: './src/uiengine/pages',
+    data: './src/uiengine/data',
+    entities: './src/uiengine/entities'
+  },
+
+  // Destination paths for the generated output.
+  target: './dist',
+
+  // Adapters are used for templating/rendering. Each adapter is a module that gets required
+  // and needs to provide functions for setup and rendering. For details see the adapters docs.
+  adapters: {
+    html: '@uiengine/adapter-html'
+  },
+
+  // Here you can configure the template that the variant preview gets embeded in.
+  template: 'uiengine.html',
+
+  ui: {
+    lang: 'en',
+    // hljs: 'atom-one-dark',
+    // customStylesFile: '/path-to-overrides.css',
+    viewports: {
+      Phone: {
+        width: 320
+      },
+      Tablet: {
+        width: 768
+      },
+      Desktop: {
+        width: 1280
+      }
+    },
+    breakpoints: {
+      XS: 320,
+      S: 560,
+      M: 768,
+      L: 960,
+      XL: 1280
+    }
+  }
+}
